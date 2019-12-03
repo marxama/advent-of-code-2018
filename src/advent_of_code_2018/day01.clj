166233c073157c48d0ff184b29c966d2bf09676c
Marcus Wangnusson <marxama@gmail.com>
2019-12-03 23:40:27 +0100

    Solve day 1

--- src/advent_of_code_2018/day01.clj
+++ src/advent_of_code_2018/day01.clj
@@ -1,1 +1,21 @@
-(ns advent-of-code-2018.day01)
+(ns advent-of-code-2018.day01
+  (:require [clojure.string :as string]))
+
+(defn parse-input []
+  (->> "resources/day01_input"
+       slurp
+       string/split-lines
+       (map read-string)))
+
+(defn solve-part-1 []
+  (reduce + (parse-input)))
+
+(defn solve-part-2 []
+  (->> (parse-input)
+       cycle
+       (reductions +)
+       (reduce (fn [r frequency]
+                 (if (r frequency)
+                   (reduced frequency)
+                   (conj r frequency)))
+               #{})))
